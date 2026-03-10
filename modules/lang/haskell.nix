b92c8525d863ec4b598cda8d483a4fe1e762da71
Raymond Chang <rchang0226@gmail.com>
2026-03-10 20:02:33 +0000

    automatically install ghc

--- modules/lang/haskell.nix
+++ modules/lang/haskell.nix
@@ -2,6 +2,7 @@
   plugins.lsp.servers = {
     hls = {
       enable = true;
+      installGhc = true;
       # Optional: extra settings for formatting or GHC options
       settings = {
         haskell.formattingProvider = "ormolu"; # or "fourmolu"
